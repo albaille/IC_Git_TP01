ecbbce196b8e5270dc0a6da0608a1962a16ceebe
AL Baille <abaille@campus-eni.fr>
2021-02-03 15:38:17 +0100

    ElementResultat

--- ficheDePaie.js
+++ ficheDePaie.js
@@ -8,7 +8,21 @@
         let input = document.getElementById(idInput);
         return parseInt(input.value);
       };
-    }    
+    }   
+
+	//Permet de gérer l'affichage des montants dans les span
+	function ElementResultat() {
+      this.Tag = function (idTag, value) {
+        let tag = document.getElementById(idTag);
+        if (tag !== null) {
+          tag.innerHTML = value;
+        }
+      };
+      this.TagMontant = function (idTag, value) {
+		let val = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(value);
+        this.Tag(idTag, val);
+      };
+    }
    
      
     function calculerSalaireNet() {
